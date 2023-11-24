8b145c4fb49181df5701f879df087331d8d0e8e0
KyrAt0r <seldpodiiiyboi@gmail.com>
2023-11-25 04:36:56 +0500

    add: ipr project

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -3,7 +3,6 @@ import {
     Route
 } from 'react-router-dom';
 import FunctionalComponent from './FunctionalComponent/FunctionalComponent';
-import ClassComponent from './ClassComponent/ClassComponent';
 import Authorization from "./Authorization/Authorization";
 import Home from "../../../react-ipr_2023/src/Pages/Home/Home";
 import Posts from "./Posts/Posts";
@@ -13,7 +12,6 @@ const Routes = () => {
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/functional" component={FunctionalComponent} />
-            <Route exact path="/class" component={ClassComponent} />
             <Route exact path="/login" component={Authorization}/>
             <Route exact path="/feed" component={Posts}/>
         </Switch>
