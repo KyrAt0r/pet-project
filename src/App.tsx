9bd30bb0b28b4e09e67cac5a6f676efabc363e61
KyrAt0r <seldpodiiiyboi@gmail.com>
2023-11-25 04:45:38 +0500

    fix for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router} from 'react-router-dom';
 
-import Header from "../../pet-project/src/components/Header/Header";
-import MainMenu from "../../pet-project/src/components/MainMenu/MainMenu";
-import Footer from "../../pet-project/src/components/Footer/Footer";
-import logo from '../../pet-project/src/assets/logoMimic.gif';
+import Header from "./components/Header/Header";
+import MainMenu from "./components/MainMenu/MainMenu";
+import Footer from "./components/Footer/Footer";
+import logo from './assets/logoMimic.gif';
 import "./styles.css";
-import Routes from "../../pet-project/src/components/Routes";
+import Routes from "./components/Routes";
 function App() {
     const menuItems = [ // Обёрнуто в JSON обьект для передачи в пропсыы
         {title: 'Главная', link: '/', id: '0'},
