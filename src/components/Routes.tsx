aeeccb45575d6836b5e13d8ee4652b5cfc8c9d86
KyrAt0r <seldpodiiiyboi@gmail.com>
2023-11-27 02:35:51 +0500

    fix роутов

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,24 +1,24 @@
-import { Switch, Route } from 'react-router-dom';
+import {
+    Switch,
+    Route
+} from 'react-router-dom';
 import FunctionalComponent from './FunctionalComponent/FunctionalComponent';
-import Authorization from './Authorization/Authorization';
-import Posts from './Posts/Posts';
-import Home from '../Pages/Home/Home.tsx';
-import Unix from './Unix/Unix.tsx';
+import Authorization from "./Authorization/Authorization";
+import Posts from "./Posts/Posts";
+import Home from "../Pages/Home/Home.tsx";
 
 const Routes = () => {
     return (
         <Switch>
-            <Route exact path={process.env.BASE_URL} component={Home} />
-            <Route exact path={`${process.env.BASE_URL}functional`} component={FunctionalComponent} />
-            <Route exact path={`${process.env.BASE_URL}login`} component={Authorization} />
-            <Route exact path={`${process.env.BASE_URL}feed`} component={Posts} />
-            <Route exact path={`${process.env.BASE_URL}unix-conv`} component={Unix} />
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/functional" component={FunctionalComponent} />
+            <Route exact path="/login" component={Authorization}/>
+            <Route exact path="/feed" component={Posts}/>
         </Switch>
     );
 };
 
 export default Routes;
-
 
 
 // <Route path="/example/:id" component={ExampleComponent} /> Передача в роут параметра
